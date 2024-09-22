0540a6f88075df4db4299f26ad781c422c4ba504
Rhimesh Lwagun <rlwagun@students.kennesaw.edu>
2024-09-22 00:21:37 -0400

    added implementation to fetch data from sd card

---
+++ components/my_sd_interface/include/my_sd_interface.h
@@ -0,0 +1,57 @@
+#ifndef MY_SD_INTERFACE_H
+#define MY_SD_INTERFACE_H
+
+#include <Arduino.h>
+
+#include <stdio.h>
+#include <string.h>
+#include <sstream>
+#include <sys/unistd.h>
+#include <sys/stat.h>
+#include <dirent.h>
+
+#include "esp_log.h"
+#include "esp_err.h"
+#include "esp_vfs_fat.h"
+
+#include "sdmmc_cmd.h"
+#include "driver/sdmmc_host.h"
+#include "vector"
+
+# ifndef MOUNT_POINT
+    #define MOUNT_POINT             "/sdcard"
+#endif 
+
+#ifndef EXAMPLE_MAX_CHAR_SIZE
+    #define EXAMPLE_MAX_CHAR_SIZE    64
+#endif
+
+static const char *SD_TAG = "SD TAG";
+static const char mount_point[] = MOUNT_POINT;
+
+void func(void);
+std::vector<std::string> split(const std::string &line, char delimiter);
+inline std::pair<int, int> compute_distance(int x1, int y1, int x2, int y2);
+bool is_noise(const std::vector<int>& current_row, const std::vector<int>& next_event, int delta);
+
+class SDCardInterface{
+private:
+    FILE *file;
+    sdmmc_card_t * card;
+    // char *mount_point; 
+    
+public:
+    SDCardInterface(void);
+    void list_dir(const char* path);
+    esp_err_t s_example_write_file(const char *path, char *data);
+    esp_err_t s_example_read_file(const char *path);
+    esp_err_t read_from_csv(const char *filename);
+    
+    // these are to read csv files
+    esp_err_t set_file_path(const char *filename, bool skip_header = true);
+    esp_err_t read_row_from_csv();
+    esp_err_t read_row_from_csv(uint8_t &x, uint8_t &y);
+    ~SDCardInterface();
+};
+
+#endif 
---
+++ components/my_sd_interface/my_sd_interface.cpp
@@ -0,0 +1,224 @@
+#include "my_sd_interface.h"
+
+void func(void){
+
+}
+
+// Function to split a line by a delimiter (comma in this case)
+std::vector<std::string> split(const std::string &line, char delimiter) {
+    std::vector<std::string> tokens;
+    std::string token;
+    std::istringstream ss(line);
+    
+    // Extract tokens separated by the delimiter
+    while (std::getline(ss, token, delimiter)) {
+        tokens.push_back(token);
+    }
+    return tokens;
+}
+
+inline std::pair<int, int> compute_distance(int x1, int y1, int x2, int y2){
+    return std::make_pair(std::abs(x1 - x2), std::abs(y1 - y2));
+}
+
+bool is_noise(const std::vector<int>& current_row, const std::vector<int>& next_event, int delta){
+    std::pair<int, int> distance = compute_distance(current_row[1], current_row[2], next_event[1], next_event[2]);
+}
+
+// class methods definition
+SDCardInterface::SDCardInterface(void){
+    esp_err_t ret;
+    esp_vfs_fat_sdmmc_mount_config_t mount_config = {
+        .format_if_mount_failed = false, 
+        .max_files = 5,
+        .allocation_unit_size = 16 * 1024
+    };
+
+    ESP_LOGI(SD_TAG, "Initializing SD card");
+    // use settings defined above to initialize SD card and mount FAT filesystem.
+    // note: esp_vfs_fat_sdmmc/sdspi_mount is all-in-one convenience functions
+    ESP_LOGI(SD_TAG, "Using SDMMC peripheral");
+
+    // By default, SD card frequency is initialized to SDMMC_FREQ_DEFAULT(20MHz)
+    sdmmc_host_t host = SDMMC_HOST_DEFAULT();
+
+    // This initializes the slot without card detect (CD) and write protect (WP) signals.
+    // Modify slot_config.gpio_cs and slot_config.gpio_wp if your board has these signals.
+    sdmmc_slot_config_t slot_config = SDMMC_SLOT_CONFIG_DEFAULT();
+
+    // set bus width to use 
+    slot_config.width = 1;
+
+    // on chips where the GPIOs used for SD card can be configured, set them in 
+    // the slot_config structure:
+    slot_config.cmd = GPIO_NUM_38;
+    slot_config.clk = GPIO_NUM_39;
+    slot_config.d0 = GPIO_NUM_40;
+
+    // slot_config.flags |= SDMMC_SLOT_FLAG_INTERNAL_PULLUP;
+
+    // mounting file system
+    ESP_LOGI(SD_TAG, "Mounting filesystem");
+    ret = esp_vfs_fat_sdmmc_mount(mount_point, &host, &slot_config, &mount_config, &this->card);
+
+    if(ret != ESP_OK){
+        if(ret == ESP_FAIL){
+            ESP_LOGE(SD_TAG, "Failed to mount filesystem."
+                "If you want the card to be formatted, set the EXAMPLE_FORMAT_IF_MOUNT_FAILED menuconfig option.");
+        } else {
+            ESP_LOGE(SD_TAG, "Failed to initialize the card (%s). "
+                     "Make sure SD card lines have pull-up resistors in place.", esp_err_to_name(ret));
+        }
+        return;
+    }
+    ESP_LOGI(SD_TAG, "Filesystem mounted");
+
+    // card has been initialized, print its properties 
+    sdmmc_card_print_info(stdout, card);
+    // list the directory and file in the card. 
+    list_dir(MOUNT_POINT);
+}
+
+void SDCardInterface::list_dir(const char * path){
+    DIR* dir = opendir(path);
+    if (dir == NULL){
+        ESP_LOGE(SD_TAG, "Failed to open directory: %s", path);
+        return;
+    }
+
+    struct dirent* entry;
+    while ((entry = readdir(dir)) != NULL){
+        // print the name of each file/directory found 
+        ESP_LOGI(SD_TAG, "Found: %s", entry->d_name);
+    }
+    closedir(dir); // close the directory when done 
+}
+
+esp_err_t SDCardInterface::s_example_write_file(const char *path, char *data){
+    ESP_LOGI(SD_TAG, "Opening file %s", path);
+    FILE *f = fopen(path, "w");
+    if (f == NULL) {
+        ESP_LOGE(SD_TAG, "Failed to open file for writing");
+        return ESP_FAIL;
+    }
+    fprintf(f, data);
+    fclose(f);
+    ESP_LOGI(SD_TAG, "File written");
+
+    return ESP_OK;
+}
+
+esp_err_t SDCardInterface::s_example_read_file(const char *path)
+{
+    ESP_LOGI(SD_TAG, "Reading file %s", path);
+    FILE *f = fopen(path, "r");
+    if (f == NULL) {
+        ESP_LOGE(SD_TAG, "Failed to open file for reading");
+        return ESP_FAIL;
+    }
+    char line[EXAMPLE_MAX_CHAR_SIZE];
+    fgets(line, sizeof(line), f);
+    fclose(f);
+
+    // strip newline
+    char *pos = strchr(line, '\n');
+    if (pos) {
+        *pos = '\0';
+    }
+    ESP_LOGI(SD_TAG, "Read from file: '%s'", line);
+
+    return ESP_OK;
+}
+
+esp_err_t SDCardInterface::read_from_csv(const char *filename){
+    FILE* file = fopen(filename, "r");
+    if (file == NULL) {
+        ESP_LOGE(SD_TAG, "Failed to open file: %s", filename);
+        return ESP_FAIL;
+    }
+
+    char line[256];
+    // while (fgets(line, sizeof(line), file)) {
+    for(int i =0; i < 10; i++){
+        fgets(line, sizeof(line), file);
+        // Convert char array to C++ string
+        std::string strLine(line);
+        // Split the line by commas
+        std::vector<std::string> tokens = split(strLine, ',');
+
+        // Print each token from the CSV line
+        for (std::vector<std::string>::iterator it = tokens.begin(); it != tokens.end(); ++it) {
+            printf("%s ", (*it).c_str());
+        }
+        printf("\n");  // Newline after each row
+    }
+
+    fclose(file);  // Close the file after reading
+    return ESP_OK;
+}
+
+// read csv files 
+esp_err_t SDCardInterface::set_file_path(const char *filename, bool skip_header){
+    this->file = fopen(filename, "r");
+    if(file == NULL){
+        ESP_LOGE(SD_TAG, "failed to open file: %s", filename);
+        return ESP_FAIL;
+    }
+    if(skip_header){
+        this->read_row_from_csv();
+        ESP_LOGI(SD_TAG, "Skipped the header row");
+    }
+    return ESP_OK;
+}
+
+esp_err_t SDCardInterface::read_row_from_csv(){
+    char line[256];
+    fgets(line, sizeof(line), this->file);
+    // convert char array to c++ string 
+    std::string strLine(line);
+    // split the line by commas 
+    std::vector<std::string> tokens = split(strLine, ',');
+
+    // print each token from the CSV line 
+    for(std::vector<std::string>::iterator it = tokens.begin(); it != tokens.end(); ++it){
+        
+        printf("s - %s ", (*it).c_str());
+        int test = atoi((*it).c_str());
+        printf("f - %d ", test);
+    }
+    printf("\n");
+    return ESP_OK;
+}  
+
+esp_err_t SDCardInterface::read_row_from_csv(uint8_t &x, uint8_t &y){
+    char line[256];
+    fgets(line, sizeof(line), this->file);
+    // convert char array to c++ string 
+    std::string strLine(line);
+    // split the line by commas 
+    std::vector<std::string> tokens = split(strLine, ',');
+
+    // print each token from the CSV line 
+    int column = 0; 
+    for(std::vector<std::string>::iterator it = tokens.begin(); it != tokens.end(); ++it){
+        uint8_t test = atoi((*it).c_str());
+        switch (column){
+        // assign value to the column
+            case 1: // column second x 
+                x = test; 
+                break;
+            case 2: // column third y 
+                y = test;
+                break;
+        }
+        column++;
+    }
+    printf("\n");
+    return ESP_OK;
+}  
+
+SDCardInterface::~SDCardInterface(){
+    // all done, umount partition and disable SDMMC peripheral 
+    // esp_vfs_fat_sdcard_format(mount_point, this->card);
+    ESP_LOGI(SD_TAG, "card unmounted");
+}
