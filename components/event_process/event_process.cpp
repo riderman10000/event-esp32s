31af658f376be691d99206e00372fe8d2102516d
Rhimesh Lwagun <rimeshlwagun10@gmail.com>
2024-09-24 08:37:42 -0400

    transferred to other components as well

---
+++ components/event_process/event_process.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include "event_process.h"
+
+
+#include "opencv2/core.hpp"
+
+void test_opencv(void)
+{
+    cv::Mat hero;
+}
---
+++ components/event_process/include/event_process.h
@@ -0,0 +1,6 @@
+#ifndef EVENT_PROCESS_H
+#define EVENT_PROCESS_H
+
+void test_opencv(void);
+
+#endif 
